3e95f3f3d603bc24facda098820db5b431956136
RandomNick7 <107753721+RandomNick7@users.noreply.github.com>
2023-10-29 18:49:26 +0200

    Lab1 Git Submission

---
+++ Lab1/extender.py
@@ -0,0 +1,13 @@
+import os
+
+target_size = 1474560
+
+fnames = ["Lab1"]
+
+for i in fnames:
+    in_f = open(i+".bin", "rb")
+    out_f = open(i+".img", "wb")
+
+    out_f.write(in_f.read())
+    bytes = b'\x00' * (target_size - os.path.getsize(i+".bin"))
+    out_f.write(bytes)
